edd32949250b65bd9a439e7fd8e196e262d6627d
Pierre-Andre Saulais <pasaulais@free.fr>
2015-03-07 20:43:31 +0000

    Fixing the build on Windows.

--- src/Material.cpp
+++ src/Material.cpp
@@ -21,11 +21,11 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <tiffio.h>
 #include <cstring>
 #include <cstdio>
 #include "Platform.h"
 #include "Material.h"
+#include <tiffio.h>
 
 Material::Material()
 {
