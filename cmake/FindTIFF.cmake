a928ea5419f9c41191518f4eec86f4a9b2f3315b
Pierre-Andre Saulais <pasaulais@free.fr>
2015-03-07 23:07:43 +0000

    Build against the TIFF installer from http://sourceforge.net/projects/gnuwin32/files/tiff/3.8.2-1/

--- cmake/FindTIFF.cmake
+++ cmake/FindTIFF.cmake
@@ -2,42 +2,38 @@
 # TIFF_INCLUDE_DIRS - GLEW include directories
 # TIFF_LIBRARIES - GLEW import library
 
-set(TIFF_INTREE_DIR "${DEMO_DEPENDS_PATH}/tiff-${TIFF_FIND_VERSION}-1")
-set(TIFF_INTREE_DLL "${TIFF_INTREE_DIR}/bin/libtiff3.dll")
-set(TIFF_ARCHIVE "${TIFF_INTREE_DIR}-1-bin.zip")
-
 if (TIFF_INCLUDE_DIRS AND TIFF_LIBRARIES)
     set(TIFF_FOUND TRUE)
 else ()
-    # Try to unpack the TIFF archive if the directory doesn't exist.
-    if(NOT EXISTS ${TIFF_INTREE_DIR} AND EXISTS ${TIFF_ARCHIVE})
-        file(MAKE_DIRECTORY ${TIFF_INTREE_DIR})
-        execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${TIFF_ARCHIVE}
-                        WORKING_DIRECTORY ${EQLI_DEPENDS_PATH})
-    endif()
-
     # Find the TIFF library
     find_path(TIFF_INCLUDE_DIRS tiff.h
-              PATHS /usr/include /usr/local/include /opt/include
-                    "${TIFF_INTREE_DIR}/include"
+              PATHS /usr/include
+                    /usr/local/include
+                    /opt/include
+                    "C:\\Program Files (x86)\\GnuWin32\\include"
                     "C:\\Program Files\\GnuWin32\\include"
     )
     find_library(TIFF_LIBRARIES libtiff
-                 PATHS /usr/lib /usr/lib64 "${TIFF_INTREE_DIR}/lib"
+                 PATHS /usr/lib
+                       /usr/lib64
+                       "C:\\Program Files (x86)\\GnuWin32\\lib"
                        "C:\\Program Files\\GnuWin32\\lib"
     )
     if(WIN32)
         find_file(TIFF_DLL_PATH libtiff3.dll
-                  PATHS "C:\\Program Files\\GnuWin32\\bin"
-                        "C:\\Program Files (x86)\\GnuWin32\\bin"
+                  PATHS "C:\\Program Files (x86)\\GnuWin32\\bin"
+                        "C:\\Program Files\\GnuWin32\\bin"
+                  NO_DEFAULT_PATH
         )
         find_file(TIFF_ZLIB_DLL_PATH zlib1.dll
-                  PATHS "C:\\Program Files\\GnuWin32\\bin"
-                        "C:\\Program Files (x86)\\GnuWin32\\bin"
+                  PATHS "C:\\Program Files (x86)\\GnuWin32\\bin"
+                        "C:\\Program Files\\GnuWin32\\bin"
+                  NO_DEFAULT_PATH
         )
         find_file(TIFF_JPEG_DLL_PATH jpeg62.dll
-                  PATHS "C:\\Program Files\\GnuWin32\\bin"
-                        "C:\\Program Files (x86)\\GnuWin32\\bin"
+                  PATHS "C:\\Program Files (x86)\\GnuWin32\\bin"
+                        "C:\\Program Files\\GnuWin32\\bin"
+                  NO_DEFAULT_PATH
         )
     endif(WIN32)
     
@@ -60,7 +56,6 @@ if(TIFF_FOUND)
         message(STATUS "Found TIFF: ${TIFF_LIBRARIES}")
     endif(NOT TIFF_FIND_QUIETLY)
     if(WIN32)
-        copy_dll_to_bin_dir("${TIFF_INTREE_DLL}")
         copy_dll_to_bin_dir("${TIFF_DLL_PATH}")
         copy_dll_to_bin_dir("${TIFF_ZLIB_DLL_PATH}")
         copy_dll_to_bin_dir("${TIFF_JPEG_DLL_PATH}")
